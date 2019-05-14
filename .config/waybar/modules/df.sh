a7ae2eeb7a0545fdf85a7eb2f52ab782bba7008f
Jonas Platte <jplatte+git@posteo.de>
2019-05-14 21:01:29 +0200

    Update waybar config

--- .config/waybar/modules/df.sh
+++ .config/waybar/modules/df.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # \xe2\x80\x89 is the utf8 representation of \u2009
-df -H --output=avail "$1" \
+text=$(df -H --output=avail "$1" \
     | tail -n1 \
-    | sed 's/\s*\([0-9.]\+\)\(\w\)\s*/\1\xe2\x80\x89\2B/'
+    | sed 's/\s*\([0-9.]\+\)\(\w\)\s*/\1\xe2\x80\x89\2B/')
+
+printf '<span></span>%s<span></span>' "$text"
