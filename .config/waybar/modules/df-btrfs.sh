81077c81143447e8487940600c11890981334da9
Jonas Platte <jplatte+git@posteo.de>
2019-08-05 22:16:01 +0200

    Remove unnecessary B from df block output to save space

--- .config/waybar/modules/df-btrfs.sh
+++ .config/waybar/modules/df-btrfs.sh
@@ -2,4 +2,4 @@
 # \xe2\x80\x89 is the utf8 representation of \u2009
 btrfs filesystem usage -H -T "$1" \
     | grep '^\s\+Free' \
-    | sed 's/[^:]\+:\s\+\([0-9]\+\)\(\.[0-9]\+\)\?\(\w\+\).*/\1\xe2\x80\x89\3/'
+    | sed 's/[^:]\+:\s\+\([0-9]\+\)\(\.[0-9]\+\)\?\(\w\)B\s\+.*/\1\xe2\x80\x89\3/'
--- .config/waybar/modules/df.sh
+++ .config/waybar/modules/df.sh
@@ -2,4 +2,4 @@
 # \xe2\x80\x89 is the utf8 representation of \u2009
 df -H --output=avail "$1" \
     | tail -n1 \
-    | sed 's/\s*\([0-9.]\+\)\(\w\)\s*/\1\xe2\x80\x89\2B/'
+    | sed 's/\s*\([0-9.]\+\)\(\w\)\s*/\1\xe2\x80\x89\2/'
